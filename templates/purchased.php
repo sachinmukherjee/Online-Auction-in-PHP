faf7017ea235a8841dc4ef309a8414843aaf9ca2
sachinmukherjee <=sachinmukherjee29@gmail.com>
2018-02-06 18:34:45 +0530

    previous files deleted

--- templates/purchased.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  session_start();
-  include 'database.php';
-  $user_id=$_SESSION['user_id'];
-
-  $query = "select * from Purchased where user_id = $user_id;";
-  $result = mysqli_query($conn, $query);
-
-?>
